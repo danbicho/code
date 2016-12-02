7e2d77ac9e6af76db74cabcb803b555f7b58b1a5
Duckyeon Kim <wheksql89@naver.com>
2016-12-02 19:08:20 +0900

    add

--- day13/ex3.js
+++ day13/ex3.js
@@ -1,16 +1,26 @@
-var picture_factory = function() {
-  $('.button').on('click', function(){
-    var value = $('.text').val();
-    get_pictures(value);
+var img = [];
 
-    $.ajax({
-      url: "https://api.imgur.com/3/gallery/search?q=" + encodeURI(value),
-      headers: { "Authorization": "Client-ID e3776adbe9c5fb9" },
-      success: function(response) {
-        console.log(response);
+var get_pictures = function(searchTerm){
+  $.ajax({
+    url: "https://api.imgur.com/3/gallery/search?q=" + encodeURI(searchTerm),
+    headers: { "Authorization": "Client-ID e3776adbe9c5fb9" },
+    success: function(response) {
+      console.log(response);
+
+      var array = response.data;
+      for(var i = 0; i < array.length; i++) {
+        var imgObj = array[i];
+        if(imgObj.type && imgObj.type.includes('image')) {
+          var link = imgObj.link;
+          var $img = $('<img>').attr('src',link);
+          $('body').append($img);
+        }
       }
-    });
+    }
   });
-
 };
-picture_factory();
+
+$('.button').on('click', function(){
+  var value = $('.text').val();
+  get_pictures(value);
+});
